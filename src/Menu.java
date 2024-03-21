1f2760800ca9a0e22cd514a113ed5ca14bf29cef
Nicolas Ortiz <nicolas.ortiz@pucpr.edu.br>
2024-03-20 21:45:39 -0300

    Tarefa 1 completa

---
+++ src/Menu.java
@@ -0,0 +1,9 @@
+public class Menu {
+    public void MenuCadastro(){
+        System.out.println("Nome: ");
+        System.out.println("Email: ");
+        System.out.println("Senha: ");
+        System.out.println("Idade: ");
+        System.out.println("CPF: ");
+    }
+}
---
+++ src/Usuario.java
@@ -0,0 +1,55 @@
+public class Usuario {
+    private String nome;
+    private String email;
+    private String senha;
+    private int idade;
+    private int cpf;
+
+    public Usuario(String nome, String email, String senha, int idade, int cpf) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.idade = idade;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+}
